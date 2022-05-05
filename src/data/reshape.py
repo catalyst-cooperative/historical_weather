a20e8b2858fcb4b65821a9c46dfc1621a7253e5c
t-desktop <tpbush@gmail.com>
2022-05-05 02:04:00 -0700

    write extract/transform for station metadata

--- src/data/reshape.py
+++ src/data/reshape.py
@@ -1,7 +1,9 @@
 from pathlib import Path
-from typing import Optional, Dict
-import pandas as pd
+from typing import Optional
+
 import numpy as np
+import pandas as pd
+from sklearn.metrics.pairwise import haversine_distances
 
 
 def _load_gsod(path: Optional[Path] = None) -> pd.DataFrame:
@@ -149,3 +151,80 @@ def get_gsod(path: Optional[Path] = None) -> pd.DataFrame:
     gsod = _load_gsod(path)
     _transform_gsod(gsod)
     return gsod
+
+
+def _extract_station_metadata(path: Optional[Path] = None) -> pd.DataFrame:
+    if path is None:
+        path = Path("../data/raw/all_gsod_stations_in_wieb_territory.csv")
+    elif isinstance(path, str):
+        path = Path(path)
+    assert path.exists()
+    dtype_dict = {
+        "usaf": str,
+        "wban": str,
+    }
+    # force ID columns to str so they don't lose zero-padding
+    stations = pd.read_csv(path, parse_dates=["begin", "end"], dtype=dtype_dict)
+    rename_dict = {
+        "lat": "latitude",
+        "lon": "longitude",
+        "elev": "elevation_ft",
+        "begin": "nominal_begin_date",
+        "end": "nominal_end_date",
+    }
+    stations.rename(columns=rename_dict, inplace=True)
+    return stations
+
+
+def _transform_station_metadata(stations: pd.DataFrame, cities: Optional[pd.DataFrame] = None) -> pd.DataFrame:
+    if cities is None:
+        cities = get_cities()
+    distance_matrix = _calculate_distance_matrix(stations, cities)
+    nearest = _nearest_city_from_matrix(distance_matrix)
+    out = pd.concat([stations, nearest], axis=1, copy=False)
+    out.loc[:, "name"] = out.loc[:, "name"].str.strip()
+    return out
+
+
+def get_cities(path: Optional[Path] = None) -> pd.DataFrame:
+    if path is None:
+        path = Path("../data/processed/priority_cities.csv")
+    elif isinstance(path, str):
+        path = Path(path)
+    assert path.exists()
+    cities = pd.read_csv(path)
+    return cities
+
+
+def _calculate_distance_matrix(stations: pd.DataFrame, cities: pd.DataFrame) -> pd.DataFrame:
+    # haversine distance gives angular distance between two points on a sphere.
+    # Convert to arc length by multiplying by earth's radius
+    earth_radius_km = 6371  # wikipedia
+
+    def degrees_to_radians(df):
+        return df * (np.pi / 180.0)
+
+    station_coords = degrees_to_radians(stations.loc[:, ["latitude", "longitude"]])
+    city_coords = degrees_to_radians(cities.loc[:, ["latitude", "longitude"]])
+    distance_matrix = haversine_distances(station_coords, city_coords) * earth_radius_km
+    distance_cols = pd.Index(cities["city"])
+    distance_matrix = pd.DataFrame(distance_matrix, columns=distance_cols, index=stations.index, dtype=np.float32)
+    return distance_matrix
+
+
+def _nearest_city_from_matrix(distance_matrix: pd.DataFrame) -> pd.DataFrame:
+    minima = distance_matrix.min(axis=1).astype(np.float32)
+    indicators = distance_matrix.eq(minima, axis=0).stack()
+    # indexing by itself filters for True. Then drop the booleans.
+    nearest = indicators.loc[indicators].reset_index(level="city").drop(columns=0)
+    nearest.rename(columns={"city": "nearest_city"}, inplace=True)
+    nearest.loc[:, "nearest_city"] = nearest.loc[:, "nearest_city"].astype(pd.CategoricalDtype())
+    nearest["distance_km"] = minima
+    return nearest
+
+
+def get_station_metadata(station_path: Optional[Path] = None, city_path: Optional[Path] = None) -> pd.DataFrame:
+    stations = _extract_station_metadata(station_path)
+    cities = get_cities(city_path)
+    stations = _transform_station_metadata(stations, cities)
+    return stations
